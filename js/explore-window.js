a4d6e58a6e345d9e506a16d9e6cabbef67fb4627
KING-REX <anijimjamesmaurice@gmail.com>
2023-05-03 13:18:34 +0100

    Rex's Commit

--- js/explore-window.js
+++ js/explore-window.js
@@ -6,11 +6,28 @@ $(document).ready(()=> {
     //     $('.explore-side-menu').toggleClass("burger-toggle");
     //     // $('.explore-side-menu').css("justify-content", "center");
     // })
-    $('.side-menu-items').mouseover(()=> {
-        $('.explore-side-menu').toggleClass("burger-toggle");  
-    }).mouseout(()=> {
-        $('.explore-side-menu').removeClass("burger-toggle");
+
+    let clickActive = false;
+
+    $('.top-half-content').click(()=> {
+        if(!clickActive) {
+            if(!$('.explore-side-menu').hasClass("burger-toggle"))
+                $('.explore-side-menu').addClass("burger-toggle");
+            clickActive = true;
+        }
+        else {
+            $('.explore-side-menu').removeClass("burger-toggle");
+            clickActive = false;
+        }
     })
+
+    $('.side-menu-items').mouseover(()=> {
+        if(!clickActive)
+            $('.explore-side-menu').toggleClass("burger-toggle");
+    }).mouseout(()=> {
+        if(!clickActive)
+            $('.explore-side-menu').removeClass("burger-toggle");
+    });
 })
 
 document.getElementsByTagName('img').alt = "";
