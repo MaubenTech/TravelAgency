8a9edbaf6ad20aa061e0dad8864abd051ee07c18
Noah-V <noahvikoo3@gmail.com>
2023-05-15 16:30:07 +0100

    Noah's Commit

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,6 +1,32 @@
 const photoJson = '/js/photo-gallery.json';
 console.log(photoJson);
 
+console.log(document.getElementById('gallery-list'))
+
+function displayPhotos(data){
+    var photoList = document.getElementById('gallery-list');
+    console.log(photoList);
+    for(var i = 0; i < data.length; i++){
+        console.log(data[i])
+        var li = document.createElement('li');
+        var figure = document.createElement('figure');
+        li.appendChild(figure);
+        console.log(figure);
+        var img = document.createElement('img');
+        img.classList.add('photo');
+        figure.appendChild(img)
+        console.log(img);
+        li.innerHTML = 
+        `
+            <figure>
+                <img src="${data[i].image}"/>
+            </figure>
+        `;
+
+        photoList.appendChild(li);
+    }
+};
+
 fetch(photoJson, {
     method: 'GET',
     headers: {
@@ -14,27 +40,4 @@ fetch(photoJson, {
 }).catch(error => console.log(error));
 
 
-console.log(document.getElementById('gallery-list'))
 
-function displayPhotos(data){
-    var photoList = document.getElementById('gallery-list');
-    console.log(photoList);
-    for(var i = 0; i < data.length; i++){
-        console.log(data[i])
-        var li = document.createElement('li');
-        var figure = document.createElement('figure');
-        li.appendChild(figure);
-        console.log(figure);
-        var img = document.createElement('img');
-        figure.appendChild(img)
-        img.classList.add('photo');
-        li.innerHTML = 
-        `
-            <figure>
-                <img src="${data[i].image}" alt="" class="photo"/>
-            </figure>
-        `;
-
-        photoList.appendChild(li);
-    }
-}
---
+++ js/slideshowPhoto.js
@@ -0,0 +1,12 @@
+
+var modal = document.getElementById('modal-slideshow');
+var images = document.getElementsByClassName('photo');
+console.log(images.length);
+var slideshow = document.getElementById('slideshow');
+
+for(var i = 0; i < images.length; i++){
+    console.log(images[i]);
+    // if(images[i].addEventListener())
+}
+
+
