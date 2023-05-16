f030bbe323095189311ee642db21f152521f9e75
KING-REX <anijimjamesmaurice@gmail.com>
2023-05-16 16:11:45 +0100

    Rex's Commit

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,21 +1,22 @@
 const modal = document.getElementById('modal-slideshow');
 const photoJson = '/js/photo-gallery.json';
-console.log(photoJson);
+// console.log(photoJson);
 
 function displayPhotos(data){
     var photoList = document.getElementById('gallery-list');
-    console.log(photoList);
+    // console.log(photoList);
+    console.log(data.length);
     for(var i = 0; i < data.length; i++){
-        console.log(data[i])
+        // console.log(data[i])
         var li = document.createElement('li');
         var figure = document.createElement('figure');
         li.appendChild(figure);
-        console.log(figure);
+        // console.log(figure);
         var img = document.createElement('img');
         img.classList.add('photo');
         img.src = data[i].image;
         figure.appendChild(img)
-        console.log(img);
+        // console.log(img);
         // li.innerHTML = 
         // `
         //     <figure>
@@ -27,9 +28,6 @@ function displayPhotos(data){
     }
 };
 
-
-
-
 fetch(photoJson, {
     method: 'GET',
     headers: {
@@ -40,51 +38,59 @@ fetch(photoJson, {
 }).then((response) => {
     console.log(response);
     displayPhotos(response);
+    addListeners(response);
 }).catch(error => console.log(error));
 
-window.onload = function(){
-    const images = document.getElementsByClassName('photo');
 
-    var slideshow = document.getElementById('slideshow');
+function addListeners(data) {
+    const images = Array.from(document.getElementsByClassName('photo'));
+    console.log(images.length);
+    console.log(document.getElementById('swiper-wrapper'));
 
-    // function displaySlides(data){
-        
-    // }
-    
-
-    for(var i = 0; i < images.length; i++){
-        console.log(images[i].src);
-        if(images[i].addEventListener("click", function(){
+    images.forEach((image)=> {
+        image.addEventListener("click", function(event) {
             modal.style.display = "flex";
             modal.style.justifyContent = "center";
             modal.style.alignItems = "center";
-            console.log("modal");
             if(modal.computedStyleMap().get('display') == "flex"){
                 console.log("gotcha");
-                for(var p = 0; p < photoJson.length; p++){
-                    var slideshowContainer = document.getElementById('swiper-wrapper');
-                    var swiperSlide = document.createElement('div');
+                var slideshowContainer = document.getElementById('swiper-wrapper');
+                var swiperSlide = document.createElement('div');
+                swiperSlide.classList.add('swiper-slide');
+                var swiperImage = document.createElement('img');
+                swiperImage.src = event.currentTarget.src;
+                swiperSlide.appendChild(swiperImage);
+                console.log("Added: " + swiperImage.src);
+                slideshowContainer.appendChild(swiperSlide);
+                
+                for(var p = 0; p < data.length; p++){
+                    console.log("Data p image: " + data[p].image);
+                    console.log("CurrentImage src: " + event.currentTarget.src);
+                    if(event.currentTarget.src.includes(data[p].image))
+                    continue;
+                    swiperSlide = document.createElement('div');
                     swiperSlide.classList.add('swiper-slide');
-                    var swiperImage = document.createElement('img');
-                    swiperImage.src = this.src;
-                    console.log(swiperImage.src);
-                    console.log(swiperImage);
+                    swiperImage = document.createElement('img');
+                    swiperImage.src = data[p].image;
                     swiperSlide.appendChild(swiperImage);
+                    console.log("Added: " + swiperImage.src);
                     slideshowContainer.appendChild(swiperSlide);
                     $(swiperImage).css({"object-fit" : "cover"})
                 }
             }else{
-                console.log('ah')
+                console.log('ah');
             }
-        }))
-        console.log(images.length)
-    } 
-   
+        })
+    })
 }
 
 var closeButton = document.getElementsByClassName('close-slideshow')[0];
 closeButton.onclick = function(){
     modal.style.display = "none";
+    
+    var slideshowContainer = document.getElementById('swiper-wrapper');
+    slideshowContainer.innerHTML = '';
+    console.log(slideshowContainer);
 }
 
     
